e5f1f8a77b45bf0d7232ea9fadf34109cb5cea6b
Marcos Vidal <marcosvidal@outlook.com>
2022-12-27 19:40:04 -0300

    asd

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -3,16 +3,18 @@
 namespace UtmBuilder.Core;
 
 public class Utm
-{/// <summary>
- /// Website Link
- /// </summary>
- /// <param name="url">Website Link </param>
- /// <param name="campaign">Campaign Details</param>
+{
+    /// <summary>
+    /// Website Link
+    /// </summary>
+    /// <param name="url">Website Link </param>
+    /// <param name="campaign">Campaign Details</param>
     public Utm(Url url, Campaign campaign)
     {
         Url = url;
         Campaign = campaign;
     }
-    public Url Url { get;  }    
-    public Campaign Campaign { get;  } 
+
+    public Url Url { get; }
+    public Campaign Campaign { get; }
 }
